d1c9166a8d8686a559fb03d9c481d0698dd20272
nozomi0712 <s.mo.19930712@gmail.com>
2020-06-12 12:00:57 +0900

    2

---
+++ 2_Rubyの文字列変換メソッドについて.rb
@@ -0,0 +1,29 @@
+# 問題.1
+# 下記の（１）を変換して（２）にするためのコードを記述してください。
+# （１） "Today" " is" " sunny"
+# （２） "Today is sunny"
+puts "Today"+" is "+"sunny"
+
+# 問題.2
+# 下記の（１）を変換して（２）にするためのコードを記述してください。
+# （１） "Ruby, Rails, JavaScript"
+# （２） ["Ruby", " Rails", " JavaScript"]
+puts "Ruby, Rails, JavaScript".split(",")
+
+# 問題.3
+# 下記の（１）を変換して（２）にするためのコードを記述してください。
+# （１） "rails"
+# （２） "Rails"
+puts "rails".capitalize
+
+# 問題.4
+# 下記の（１）を変換して（２）にするためのコードを記述してください。
+# （１） "abcde"
+# （２） "edcba"
+puts "abcde".reverse 
+
+# 問題.5
+# 下記の（１）を変換して（２）にするためのコードを記述してください。
+# （１） "Hello, world"
+# （２） "HELLO, WORLD"
+puts "Hello, world".upcase
